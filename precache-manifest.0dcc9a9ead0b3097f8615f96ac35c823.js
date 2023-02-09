af5ba6b83bcd77c297dab9a44ef659f29e4e80f0
saubby <saubby27@gmail.com>
2023-02-10 00:12:41 +0530

    Updates

---
+++ precache-manifest.0dcc9a9ead0b3097f8615f96ac35c823.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d99a4552d3705d09b5c6287daf04c5",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "f2a5af025813bc7663c8",
+    "url": "/React-Portfolio/static/css/main.adcc9ffd.chunk.css"
+  },
+  {
+    "revision": "bdf0d5c6a6f3229ef2a4",
+    "url": "/React-Portfolio/static/js/2.a485859f.chunk.js"
+  },
+  {
+    "revision": "5bdf033543a32910296a9bdf161e6f5d",
+    "url": "/React-Portfolio/static/js/2.a485859f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6fb096d9b70d41f103d",
+    "url": "/React-Portfolio/static/js/3.c9e9ebb7.chunk.js"
+  },
+  {
+    "revision": "f2a5af025813bc7663c8",
+    "url": "/React-Portfolio/static/js/main.234b90f8.chunk.js"
+  },
+  {
+    "revision": "703a258f152f80a842ec",
+    "url": "/React-Portfolio/static/js/runtime-main.40db2d82.js"
+  },
+  {
+    "revision": "0b1c97c69bb139833eee2fcd4561313d",
+    "url": "/React-Portfolio/static/media/Resume of Sauban Hussain Malek.0b1c97c6.pdf"
+  },
+  {
+    "revision": "cae9189cc9b52acb086c2c9d2ec818c4",
+    "url": "/React-Portfolio/static/media/mypicture.cae9189c.jpg"
+  },
+  {
+    "revision": "d56c044b049d3de851cee48c3fe1e812",
+    "url": "/React-Portfolio/static/media/myportfolio.d56c044b.png"
+  },
+  {
+    "revision": "218f7fbb008d6c08d6e09442debfb328",
+    "url": "/React-Portfolio/static/media/nt.218f7fbb.png"
+  },
+  {
+    "revision": "defd4a46069ca434cb4a181d81f7eaa0",
+    "url": "/React-Portfolio/static/media/pg.defd4a46.png"
+  },
+  {
+    "revision": "5fc571241eda207526dbc2d403cfdbed",
+    "url": "/React-Portfolio/static/media/tpg.5fc57124.png"
+  },
+  {
+    "revision": "ec992de619abe624e3a1afd69db2167f",
+    "url": "/React-Portfolio/static/media/wd.ec992de6.png"
+  },
+  {
+    "revision": "e336ce55fa8b9575294aa34675ac3823",
+    "url": "/React-Portfolio/static/media/wds.e336ce55.png"
+  }
+]);
